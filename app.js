05bb0da3c7661bbed55790cb12fd7fd30bd7377b
Mia <5811563+miasa@users.noreply.github.com>
2019-05-05 21:44:30 +0300

    all the rest

--- app.js
+++ app.js
@@ -1,21 +1,9 @@
 const express = require('express');
 const compress = require('compression');
 const helmet = require('helmet');
-const fetch = require('node-fetch');
 
-const HSL_GRAPHQL_URL = 'https://api.digitransit.fi/routing/v1/routers/hsl/index/graphql';
 const port = process.env.PORT || 3000;
 const app = express();
-
-const wantedStations = [
-  '021', //Töölönlahdenkatu
-  '022', //Rautatientori, länsi
-  '023', //Kiasma
-  '006', //Hietalahdentori
-  '067', //Perämiehenkatu
-  '005' //Sepänkatu
-];
-let stationCache = {};
 
 //Harden just a little bit
 app.use(compress());
@@ -24,54 +12,7 @@ app.use(helmet());
 //Start app
 app.listen(port, () => {
   console.log(`Oravanpyörä listening on http://localhost:${port}`);
-  setInterval(refreshBikeStationCache, 5 * 1000);
-  refreshBikeStationCache();
 });
 
 //index
 app.use(express.static('./public')); //, {maxAge: 30 * 60 * 1000}))
-
-//API
-app.get('/api/stations', (req, res) => {
-  res.setHeader('Cache-Control', 'public, max-age=5');
-  res.json({
-    bikeStations: stationCache
-  });
-});
-
-function refreshBikeStationCache() {
-  const query = `
-    {
-      bikeRentalStations {
-        stationId
-        lat
-        lon
-        name
-        bikesAvailable
-        spacesAvailable
-        state
-      }
-    }
-  `;
-
-  fetch(HSL_GRAPHQL_URL, {
-    method  : 'post',
-    body    : JSON.stringify({query: query}),
-    headers : { 'Content-Type': 'application/json'},
-  })
-  .then(res => res.json())
-  .then(res => {
-    stationCache = res.data.bikeRentalStations
-      .map(station => {
-        if(station.state === 'Station off') {
-          station.active = false;
-        } else {
-          station.active = true;
-        }
-        delete station.state;
-        return station;
-      })
-      .filter(station => station.active)
-      .filter(station => wantedStations.includes(station.stationId));
-  });
-}
--- public/js/main.js
+++ public/js/main.js
@@ -1,15 +1,31 @@
 var defaultZoom = 13.5;
 var markers = [];
+var client;
+
+const HSL_GRAPHQL_URL = 'https://api.digitransit.fi/routing/v1/routers/hsl/index/graphql';
+const HSL_MQTT_URL = 'mqtts://mqtt.hsl.fi:443/';
+const HSL_MQTT_TRAM_URL = '/hfp/v1/journey/ongoing/tram/#';
+
+const wantedTramLines = ['1','3','6'];
+const wantedBikeStations = [
+  '021', //Töölönlahdenkatu
+  '022', //Rautatientori, länsi
+  '023', //Kiasma
+  '006', //Hietalahdentori
+  '067', //Perämiehenkatu
+  '005'  //Sepänkatu
+];
 
 function initialiseMap() {
   mapboxgl.accessToken = 'pk.eyJ1IjoibWlhc2EiLCJhIjoiY2p1dTQyczF6MDcyeTN5bm8xbWFoazBkdiJ9.BI5xVCsJISLyzFAG3W2V-A';
+
   map = new mapboxgl.Map({
     container : 'map-canvas',
-    style     : 'mapbox://styles/mapbox/streets-v11',
+    style     : 'mapbox://styles/miasa/cjvb962rs12y61fkxql06jhj2',
     zoom      : defaultZoom,
     center    : [24.9399946, 60.1729721]
   });
-  //map.on('moveend', toggleMarkerVisibility);
+
   moveToUserLocation();
 }
 
@@ -28,37 +44,17 @@ function moveToUserLocation() {
   }, 800);
 }
 
-/*
-function toggleMarkerVisibility() {
-  var bounds = map.getBounds();
-  console.log('map bounds', bounds);
-  markers.forEach(function(marker) {
-    var markerObject = marker.markerObject;
-    var markerPos = markerObject.getLngLat();
-    markerObject.setVisible(inBounds(markerObject.getLngLat()));
-    console.log('marker', marker, markerPos, inBounds(markerPos));
-  })
+function getStationStatus(bikesAvailable) {
+  return bikesAvailable >= 3 ? 'plenty' : 'few';
 }
-
-function inBounds(pos) {
-  const bounds = map.getBounds();
-  const lng = (pos.lng - bounds['_ne']['lng']) * (pos.lng - bounds['_sw']['lng']) < 0;
-  const lat = (pos.lat - bounds['_ne']['lat']) * (pos.lat - bounds['_sw']['lat']) < 0;
-  return lng && lat;
-}
-
-function setMarkerVisibility(marker) {
-  marker.markerObject.addTo(map);
-}
-*/
 
 function createStation(stationObject) {
   var bikesAvailable = parseInt(stationObject.bikesAvailable);
   var labelContent = '<div class="count">' + bikesAvailable + '</div>';
-  var stationStatus = bikesAvailable >= 3 ? 'plenty' : 'few';
+  var stationStatus = getStationStatus(bikesAvailable);
 
   var el = document.createElement('div');
-  el.className = `bikestation-marker status-${stationStatus}`;
+  el.className = `bikestation-marker status-${stationStatus} station-${stationObject.stationId}`;
   el.innerHTML = labelContent;
 
   var markerObject = new mapboxgl.Marker({
@@ -74,31 +70,137 @@ function createStation(stationObject) {
 
   markers.push(stationMarker);
   stationMarker.markerObject.addTo(map);
-  //setMarkerVisibility(stationMarker);
 }
 
+function createTram(tramObject) {
+  var el = document.createElement('div');
+  el.className = `tram-marker tram-` + tramObject.id;
+  el.innerHTML = '<div class="line">' + tramObject.line + '</div><div class="direction"></div>';
 
-function getJSON(url, callback) {
-  var request = new XMLHttpRequest();
-  request.open('GET', url, true);
+  var markerObject = new mapboxgl.Marker({
+    element : el,
+    anchor  : 'center'
+  }).setLngLat([tramObject.lon, tramObject.lat]);
 
-  request.onload = function() {
-    if(this.status >= 200 && this.status < 400) {
-      var data = JSON.parse(this.response);
-      callback(data);
-    }
+  var tramnMarker = {
+    markerObject : markerObject,
+    id           : tramObject.id,
+    type         : 'tram'
   };
 
-  request.send();
+  markers.push(tramnMarker);
+  tramnMarker.markerObject.addTo(map);
+}
+
+function updateTram(tramObject) {
+  var element = document.querySelector('.tram-' + tramObject.id);
+  if(element) {
+      var markerObject = getMarker(tramObject.id).markerObject;
+      markerObject.setLngLat([tramObject.lon, tramObject.lat]);
+      updateTramDirection(element, tramObject.hdg);
+  }
+}
+
+function updateTramDirection(element, direction) {
+  var directionElement = element.querySelector('.direction');
+  if(typeof direction === 'number') {
+    directionElement.style.transform = 'rotate(' + direction + 'deg)';
+    directionElement.classList.remove('hidden');
+  } else {
+    directionElement.classList.add('hidden');
+  }
+}
+
+function markerExists(id) {
+  return markers.some(marker => marker.id === id);
+}
+
+function getMarker(id) {
+  const index = markers.findIndex(marker => marker.id === id);
+  return markers[index];
+}
+
+function createOrUpdateTram(tramObject) {
+  if(markerExists(tramObject.id)) {
+    updateTram(tramObject);
+  } else {
+    createTram(tramObject);
+  }
+}
+
+function fetchBikeStations() {
+  const query = `
+    {
+      bikeRentalStations {
+        stationId
+        lat
+        lon
+        name
+        bikesAvailable
+        spacesAvailable
+        state
+      }
+    }
+  `;
+
+  return fetch(HSL_GRAPHQL_URL, {
+    method  : 'post',
+    body    : JSON.stringify({query: query}),
+    headers : {'Content-Type': 'application/json'},
+  })
+  .then(res => res.json())
+  .then(res => {
+    return res.data.bikeRentalStations
+      .map(station => {
+        if(station.state === 'Station off') {
+          station.active = false;
+        } else {
+          station.active = true;
+        }
+        delete station.state;
+        return station;
+      })
+      .filter(station => station.active)
+      .filter(station => wantedBikeStations.includes(station.stationId));
+  });
+}
+
+function createOrUpdateBikeStations(stationObjects) {
+  stationObjects.forEach(stationObject => {
+    if(markerExists(stationObject.stationId)) {
+      updateBikeStation(stationObject);
+    } else {
+      createStation(stationObject);
+    }
+  });
+}
+
+function updateBikeStation(stationObject) {
+  var element = document.querySelector('.station-' + stationObject.stationId);
+  if(element) {
+    const bikesAvailable = parseInt(stationObject.bikesAvailable);
+    const stationStatus = getStationStatus(bikesAvailable)
+    element.querySelector('.count').innerHTML = bikesAvailable;
+    element.classList.forEach(className => {
+      if(className.startsWith('station-')) {
+        element.classList.remove(className);
+      }
+    });
+    element.classList.add(`station-${stationStatus}`);
+  }
+}
+
+function initBikeStations() {
+  fetchBikeStations().then(data => createOrUpdateBikeStations(data));
+  window.setInterval(() => {
+    fetchBikeStations().then(data => createOrUpdateBikeStations(data));
+  }, 10000);
 }
 
 function initializeApp() {
   initialiseMap();
-
-  getJSON('/api/stations', function(data) {
-    console.log('api data', data);
-    data.bikeStations.map(createStation);
-  });
+  initTrams();
+  initBikeStations();
 }
 
 function ready(fn) {
@@ -108,5 +210,38 @@ function ready(fn) {
     document.addEventListener('DOMContentLoaded', fn);
   }
 }
+
+
+function initTrams() {
+  if(client) {
+    client.end(true);
+  }
+
+  client = mqtt.connect(HSL_MQTT_URL);
+
+  client.on("connect", function() {
+    client.subscribe(HSL_MQTT_TRAM_URL);
+  });
+
+  client.on("message", function(topic, payload, packet) {
+    var VP = JSON.parse(payload.toString()).VP;
+
+    if(!VP.lat || !VP.long) {
+      return;
+    }
+
+    if(wantedTramLines.includes(VP.desi)) {
+      createOrUpdateTram({
+        id   : VP.oper + '-' + VP.veh,
+        line : VP.desi,
+        lon  : VP.long,
+        lat  : VP.lat,
+        hdg  : VP.hdg
+      });
+    }
+  });
+};
+
+/****************************/
 
 ready(initializeApp);
